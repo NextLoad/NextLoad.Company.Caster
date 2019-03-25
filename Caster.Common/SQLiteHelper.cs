217ab093289cfecb64be3cdfae2cb6ab237ddde4
wuhuajiecsharp <17681550310@163.com>
2019-03-25 19:33:01 +0800

    增加第三方类库Sqlite

--- Caster.Common/SQLiteHelper.cs
+++ Caster.Common/SQLiteHelper.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Caster.Common
 {
-    class SQLiteHelper
+    public static class SQLiteHelper
     {
+        /// <summary>
+        /// 获取数据库连接字符串
+        /// </summary>
+        /// <returns></returns>
+        private static string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["SqliteConn"].ConnectionString;
+        }
+        public static int ExecuteScalar(string sqlText,)
     }
 }
