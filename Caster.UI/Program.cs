e3ba465ea6c1ca4e143c00cd94e4011ce22d0042
NextLoad <1071623997@qq.com>
2019-03-27 09:15:43 +0800

    another

--- Caster.UI/Program.cs
+++ Caster.UI/Program.cs
@@ -15,6 +15,7 @@ namespace Caster.UI
         static void Main()
         {
             //另一个github账号
+            //另一个github账号
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmLogin());
